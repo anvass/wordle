438fad8977bcd0c325c22c6c6dcd0512a587a387
anvass <nastya_0809@mail.ru>
2025-03-31 17:23:28 +0300

    style: grid component

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,21 +1,8 @@
 import { GameState } from '../types';
 
-/*
-export interface GameState {
-  enteredWords: string[];
-  currentWord: string;
-  guessWord: string;
-}
-
-*/
-
 interface GridProps {
   gameState?: GameState;
 }
-
-// @TODO: необходимо отрисовать в сетке
-// - ранее введенные слова
-// - текущее вводимое слово
 
 function Grid({ gameState }: GridProps) {
   const ROWS_COUNT = 6;
@@ -24,33 +11,35 @@ function Grid({ gameState }: GridProps) {
   const { enteredWords } = gameState;
 
   return (
-    <div
-      className={`grid grid-cols-${COLS_COUNT} grid-rows-${ROWS_COUNT} gap-2`}
-    >
-      {enteredWords.map((enteredWord: string) =>
-        enteredWord.split('').map(
-          (letter, index: number) =>
-            index < ROWS_COUNT - 1 && (
-              <div
-                className="cell border-solid border-2 border-black rounded-sm h-16"
-                key={index}
-              >
-                {letter}
-              </div>
-            )
-        )
-      )}
-
-      {ROWS_COUNT - enteredWords.length > 0 &&
-        Array.from(
-          { length: (ROWS_COUNT - enteredWords.length) * COLS_COUNT },
-          (_, index) => (
-            <div
-              className="border-solid border-2 border-black rounded-sm h-16"
-              key={index}
-            ></div>
+    <div className="flex justify-center">
+      <div
+        className={`grid grid-cols-${COLS_COUNT} grid-rows-${ROWS_COUNT} gap-2 p-3 box-border w-full max-w-[500px]`}
+      >
+        {enteredWords.map((enteredWord: string) =>
+          enteredWord.split('').map(
+            (letter, index: number) =>
+              index < ROWS_COUNT - 1 && (
+                <div
+                  className="border-2 px-1 py-2 text-center text-3xl sm:text-4xl uppercase font-bold border-neutral-300 rounded-sm"
+                  key={index}
+                >
+                  <p>{letter}</p>
+                </div>
+              )
           )
         )}
+
+        {ROWS_COUNT - enteredWords.length > 0 &&
+          Array.from(
+            { length: (ROWS_COUNT - enteredWords.length) * COLS_COUNT },
+            (_, index) => (
+              <div
+                className="border-2 px-1 py-2 text-center text-3xl sm:text-4xl uppercase font-bold border-neutral-300 rounded-sm"
+                key={index}
+              ></div>
+            )
+          )}
+      </div>
     </div>
   );
 }
