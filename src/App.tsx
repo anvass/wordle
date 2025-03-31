5ad40b35f6be73ea8b5d4fca3868fceba7b5542d
anvass <nastya_0809@mail.ru>
2025-03-31 20:36:47 +0300

    feat: entering&removing new word

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,47 @@ function App() {
     guessWord: 'пилот',
   });
 
-  const handleKeyboardEnter = (key: string) => {
+  const handleLetterEnter = (key: string) => {
     // - когда ввожу буквы, изменяем currentWord и проверяем его длину
-    // - когда нажимаю на крестик, изменяем currentWord
+
     // когда нажимаю на ввод
     // - проверяем можно ли добавить в enteredWords
     // - проверяем победили ли мы
 
-
-
-    
+    if (gameState.currentWord.length < 5) {
+      setGameState((prevState) => ({
+        ...prevState,
+        currentWord: prevState.currentWord + key,
+      }));
+    }
   };
+
+  // - когда нажимаю на крестик, изменяем currentWord
+  const handleLetterRemove = () => {
+    if (gameState.currentWord.length > 0) {
+      setGameState((prevState) => ({
+        ...prevState,
+        currentWord: prevState.currentWord.slice(0, -1),
+      }));
+    }
+  };
+
+  const handleWordEnter = (key: string) => {};
 
   return (
     <>
+      <br />
       <h1 className="text-center text-3xl font-bold uppercase">Wordle</h1>
+      <br />
       <Grid gameState={gameState} />
-      <Keyboard gameState={gameState} onEnter={handleKeyboardEnter} />
+      <br />
+      <Keyboard
+        gameState={gameState}
+        onLetterEnter={handleLetterEnter}
+        onLetterRemove={handleLetterRemove}
+        onWordEnter={handleWordEnter}
+      />
+      <br />
     </>
   );
 }
