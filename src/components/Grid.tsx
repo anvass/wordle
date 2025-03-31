3686e00c8d690bc6104814e923086022e10877e4
anvass <nastya_0809@mail.ru>
2025-03-31 20:35:20 +0300

    feat: grid update

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -8,17 +8,32 @@ function Grid({ gameState }: GridProps) {
   const ROWS_COUNT = 6;
   const COLS_COUNT = 5;
 
-  const { enteredWords } = gameState;
+  const { enteredWords, currentWord } = gameState;
 
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center w-full">
       <div
-        className={`grid grid-cols-${COLS_COUNT} grid-rows-${ROWS_COUNT} gap-2 p-3 box-border w-full max-w-[500px]`}
+        className={`grid grid-cols-${COLS_COUNT} grid-rows-${ROWS_COUNT} gap-2 w-full max-w-[95%] sm:max-w-xl`}
       >
-        {enteredWords.map((enteredWord: string) =>
-          enteredWord.split('').map(
-            (letter, index: number) =>
-              index < ROWS_COUNT - 1 && (
+        {enteredWords.length > 0 &&
+          enteredWords.map((enteredWord: string) =>
+            enteredWord.split('').map(
+              (letter, index: number) =>
+                index < COLS_COUNT && (
+                  <div
+                    className="border-2 px-1 py-2 text-center text-3xl sm:text-4xl uppercase font-bold border-neutral-300 rounded-sm"
+                    key={index}
+                  >
+                    <p>{letter}</p>
+                  </div>
+                )
+            )
+          )}
+
+        {currentWord.length > 0 &&
+          currentWord.split('').map(
+            (letter: string, index: number) =>
+              index < COLS_COUNT && (
                 <div
                   className="border-2 px-1 py-2 text-center text-3xl sm:text-4xl uppercase font-bold border-neutral-300 rounded-sm"
                   key={index}
@@ -26,17 +41,42 @@ function Grid({ gameState }: GridProps) {
                   <p>{letter}</p>
                 </div>
               )
-          )
-        )}
+          )}
 
-        {ROWS_COUNT - enteredWords.length > 0 &&
+        {currentWord.length > 0 &&
+          currentWord.length < COLS_COUNT &&
           Array.from(
-            { length: (ROWS_COUNT - enteredWords.length) * COLS_COUNT },
+            { length: COLS_COUNT - currentWord.length },
             (_, index) => (
               <div
                 className="border-2 px-1 py-2 text-center text-3xl sm:text-4xl uppercase font-bold border-neutral-300 rounded-sm"
                 key={index}
-              ></div>
+              >
+                <p>&nbsp;</p>
+              </div>
+            )
+          )}
+
+        {currentWord.length == 0 &&
+          Array.from({ length: COLS_COUNT }, (_, index) => (
+            <div
+              className="border-2 px-1 py-2 text-center text-3xl sm:text-4xl uppercase font-bold border-neutral-300 rounded-sm"
+              key={index}
+            >
+              <p>&nbsp;</p>
+            </div>
+          ))}
+
+        {ROWS_COUNT - enteredWords.length - 1 > 0 &&
+          Array.from(
+            { length: (ROWS_COUNT - enteredWords.length - 1) * COLS_COUNT },
+            (_, index) => (
+              <div
+                className="border-2 px-1 py-2 text-center text-3xl sm:text-4xl uppercase font-bold border-neutral-300 rounded-sm"
+                key={index}
+              >
+                <p>&nbsp;</p>
+              </div>
             )
           )}
       </div>
