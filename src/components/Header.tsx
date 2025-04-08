937008b3fcfdf52669f3db44b184014f18307009
anvass <nastya_0809@mail.ru>
2025-04-08 19:05:49 +0300

    style: update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,17 @@ import { BsQuestionCircleFill } from 'react-icons/bs';
 
 function Header() {
   return (
-    <header className="flex justify-between items-center mb-5 p-5">
-      <div></div>
-      <div>
-        <h1 className="text-center text-5xl md:text-4xl uppercase">Вордл</h1>
-      </div>
-      <div className="flex justify-end gap-2 items-center">
-        <button className="text-gray-500 text-[30px] cursor-pointer hover:text-black">
-          <BsQuestionCircleFill />
-        </button>
+    <header>
+      <div className="container flex justify-between items-center mb-5 p-5">
+        <div></div>
+        <div>
+          <h1 className="text-center text-5xl md:text-4xl uppercase">Вордл</h1>
+        </div>
+        <div className="flex justify-end gap-2 items-center">
+          <button className="text-gray-500 text-[30px] cursor-pointer hover:text-black">
+            <BsQuestionCircleFill />
+          </button>
+        </div>
       </div>
     </header>
   );
