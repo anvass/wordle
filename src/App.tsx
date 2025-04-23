b3bdce18a516a872133ed85defac5f7823244fb4
anvass <nastya_0809@mail.ru>
2025-04-23 12:51:00 +0300

    feat: add content to help modal

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { COLS_COUNT, ROWS_COUNT } from './constants';
 import Modal from './components/Modal';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { AiOutlineEnter } from 'react-icons/ai';
+import { FaDeleteLeft } from 'react-icons/fa6';
 
 type ModalName = 'help' | 'success' | 'failed' | 'config';
 
@@ -128,8 +130,74 @@ function ModalContainer({
   }
   if (modalName === 'help') {
     return (
-      <Modal onClose={() => setModalName(null)} title={'Help'}>
-        Help
+      <Modal onClose={() => setModalName(null)} title={'Помощь'}>
+        <div className="text-left flex flex-col gap-4">
+          <div>
+            <p>
+              Вордл - это простая и увлекательная головоломка, в которой нужно
+              угадать слово из 5 букв за 6 попыток.
+            </p>
+          </div>
+          <div>
+            <p className="mb-2 font-semibold text-center">Как играть?</p>
+            <ol>
+              <li>Введите любое слово из пяти букв</li>
+              <li>
+                Нажмите
+                <span className="inline-block px-1 align-middle">
+                  <FaDeleteLeft />
+                </span>
+                , если ошиблисть при вводе
+              </li>
+              <li>
+                Нажмите
+                <span className="inline-block px-1 align-middle">
+                  <AiOutlineEnter />
+                </span>
+                для проверки введенного слова
+              </li>
+              <li>
+                <p>
+                  После каждой попытки буквы будут подсвечены разными цветами:
+                </p>
+                <ul>
+                  <li>
+                    🟢 Зелёный цвет означает, что буква находится на правильном
+                    месте в слове
+                  </li>
+                  <li>
+                    🟡 Жёлтый цвет показывает, что буква есть в слове, но стоит
+                    не там
+                  </li>
+                  <li>
+                    ⬜ Серый цвет указывает, что такой буквы нет в загаданном
+                    слове
+                  </li>
+                </ul>
+              </li>
+            </ol>
+          </div>
+          <div>
+            <p className="mb-2 font-semibold text-center">Условия победы</p>
+            <ul>
+              <li>
+                Вы выигрываете, если угадаете слово раньше, чем закончатся
+                попытки
+              </li>
+              <li>Если все попытки исчерпаны, игра заканчивается проигрышем</li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex mt-4">
+          <button
+            onClick={() => {
+              onReset();
+            }}
+            className="w-full p-3 rounded-md text-lg bg-blue-600 text-white uppercase hover:bg-blue-700 transition-colors cursor-pointer"
+          >
+            Все понятно
+          </button>
+        </div>
       </Modal>
     );
   }
