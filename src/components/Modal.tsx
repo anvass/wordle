f70d6ecb2d9fd82fbe5ba395a6dbec42a283ab42
anvass <nastya_0809@mail.ru>
2025-04-28 11:37:47 +0300

    style: modal alignment

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,8 +13,8 @@ function Modal({ onClose, children, title }: ModalProps) {
     <div className="fixed inset-0 z-50 flex items-center justify-center transition-opacity duration-300">
       <div className="fixed inset-0 bg-gray-500/75 transition-opacity" />
       <div className="fixed inset-0 z-100 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="text-center relative transform overflow-hidden rounded-md bg-white shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="text-center relative transform overflow-hidden rounded-md bg-white shadow-xl transition-all sm:my-8 w-full sm:max-w-lg">
             {title && (
               <div className="py-4 border-b">
                 <h2 className="font-semibold text-lg">{title}</h2>
